6a36cec8f4b38b341af2711f0a4235839f8ceebe
Jair Yara <jair.yara11@gmail.com>
2026-03-30 10:15:28 -0500

    Switch to static output mode in Astro config and add Wrangler compatibility flag.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jairyara.dev',
-	output: 'hybrid',
+	output: 'static',
 	adapter: cloudflare({ platformProxy: { enabled: true } }),
 	integrations: [mdx(), sitemap()],
 	  vite: {
